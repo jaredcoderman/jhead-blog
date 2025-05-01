9f327ce9cae4541f7eef9d81415d7275c9a36dd0
Jared Head <jaredheadd@gmail.com>
2025-05-01 19:19:23 -0400

    manual install Hugo 0.146.0 for Vercel

--- install-hugo.sh
+++ install-hugo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 HUGO_VERSION="0.146.0"
 
-wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
-tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
-mv hugo /usr/local/bin/hugo
+# Download and extract Hugo
+curl -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz | tar -xz
+
+# Move Hugo binary to a folder we can reference
+mkdir -p .bin
+mv hugo .bin/hugo
